25fe0a7ffe2ae20ce60c3b24ffcad951920d1aaa
Tony <NodeToNowhere@gmail.com>
2022-04-11 19:52:14 +0100

    Bool to String + tests

---
+++ BoolToString.js
@@ -0,0 +1,9 @@
+// export function booleanToString(b){
+//   return b ? "true" : "false";
+// }
+
+
+export function booleanToString(b){
+    return b.toString();
+  }
+  
---
+++ test/BoolToString.test.js
@@ -0,0 +1,18 @@
+import { booleanToString } from '../BoolToString.js';
+import chai from 'chai';
+const Test = chai.assert;
+
+
+describe("Solution", () =>{
+    it("Fixed Tests", function() {
+      Test.strictEqual(booleanToString(true), "true", 'When we pass in true, we want the string "true" as output');
+      Test.strictEqual(booleanToString(false), "false", 'When we pass in false, we want the string "false" as output');
+      Test.strictEqual(booleanToString(false), "false", 'When we pass in false, we want the string "false" as output');
+    });
+    // it("Random Tests", function() {
+    //   Array(40).fill(0).forEach(() => {
+    //     const val = Math.random() < 0.5, exp = val ? "true" : "false"
+    //     Test.strictEqual(booleanToString(val), exp, `When we pass in ${exp}, we want the string "${exp}" as output`)
+    //   })
+    // })
+  });
