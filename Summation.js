3c69fc7bdeb1d500686f81019eae2733081aa27d
Tony <NodeToNowhere@gmail.com>
2022-04-13 21:54:47 +0100

    summation

---
+++ Summation.js
@@ -0,0 +1,11 @@
+export function summation(num) {
+    var result = num;
+     if (num === 0 || num === 1) 
+       return 1; 
+     while (num > 1) { 
+       num--;
+       result += num;
+     }
+     return result;
+   }
+
---
+++ test/Summation.test.js
@@ -0,0 +1,12 @@
+
+import { summation } from '../summation.js';
+import chai from 'chai';
+const assert = chai.assert;
+
+describe('summation', function () {
+    it('should return the correct total', function () {
+      assert.strictEqual(summation(1), 1)
+      assert.strictEqual(summation(8), 36)
+    })
+  })
+  
