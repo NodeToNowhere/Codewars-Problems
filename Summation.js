cb42c2e9c0433168c46e7fbc3f6baa712b7de551
Tony <NodeToNowhere@gmail.com>
2022-04-13 22:01:54 +0100

    refactor summation

--- Summation.js
+++ Summation.js
@@ -1,9 +1,15 @@
+// export function summation(num) {
+//   var result = num;
+//   if (num === 0 || num === 1) return 1;
+//   while (num > 1) {
+//     num--;
+//     result += num;
+//   }
+//   return result;
+// }
+
+// const summation = n => n * (n + 1) / 2; //arrow functions are godlike
+
 export function summation(num) {
-  var result = num;
-  if (num === 0 || num === 1) return 1;
-  while (num > 1) {
-    num--;
-    result += num;
-  }
-  return result;
+    return num * (num+1) / 2
 }
