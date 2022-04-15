94fb8080815e24f5f414f3b70aa9f93ce7bd6e10
Tony <NodeToNowhere@gmail.com>
2022-04-15 23:29:09 +0100

    Tidy

--- Milliseconds.js
+++ Milliseconds.js
@@ -1,4 +1,4 @@
-export const TimeToMilli = (h, m, s) => h * 3600000 + m * 60000 + s * 1000; // implicit return arrow functions are awesome.
+export const TimeToMilli = (h, m, s) => h * 3600000 + m * 60000 + s * 1000; // -- Only implicit when body not wrapped in brackets
 
 // function past(h, m, s){
 //     return ((h*3600)+(m*60)+s)*1000;  // alternate cleaner math
