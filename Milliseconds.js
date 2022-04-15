8985cf281554a754bec9d96281db7d2c8f553a07
Tony <NodeToNowhere@gmail.com>
2022-04-15 23:25:32 +0100

    Tidy math option

--- Milliseconds.js
+++ Milliseconds.js
@@ -1,1 +1,5 @@
 export const TimeToMilli = (h, m, s) => h * 3600000 + m * 60000 + s * 1000; // implicit return arrow functions are awesome.
+
+// function past(h, m, s){
+//     return ((h*3600)+(m*60)+s)*1000;  // alternate cleaner math
+//   }
