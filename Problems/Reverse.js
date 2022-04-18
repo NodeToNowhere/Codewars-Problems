fabd5638fd5d288004bdc001cda5e8771cc3520a
Tony <NodeToNowhere@gmail.com>
2022-04-18 22:39:40 +0100

    reverse string

--- Problems/Reverse.js
+++ Problems/Reverse.js
@@ -4,3 +4,5 @@
 
 export const reverse = n => n.split("").reverse().join("");
 
+
+// const solution = s => [...s].reverse().join('')  // Cool solution that uses spread syntax. (operates on all items in array)
