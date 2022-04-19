c8e92dbe1cf48fc6541f04ec4155c64f1a7d19b1
Tony <NodeToNowhere@gmail.com>
2022-04-19 01:42:54 +0100

    Right... smh

--- Problems/ReverseWordInString.js
+++ Problems/ReverseWordInString.js
@@ -1,16 +1,17 @@
-// @ts-check
-export function reverseWords(str) {
-  const words = str.split(" "); // array of words
+// export function reverseWords(str) {
 
-  console.log(words);
+//   const spin = str.split(" ");
+//   const spinMap = spin.map((x) => x.split("").reverse().join(""));
+//   return spinMap.join(" ") ;
 
-  // swapWord((word) => word.split("").reverse().join(""))
+// }
 
-  // const result = words.forEach(swapWord)
+// export const reverseWords = (str) => str.split(" ").map( w => w.split("").reverse().join("")).join(" ")   // refactored with implicit arrow fn
 
-  return words.forEach((word) => word.split("").reverse().join(""));
-}
+export const reverseWords = (str) => str.split("").reverse().join("").split(" ").reverse().join(" ") // solution from CodeWars - you are not clever Tony... 
 
-console.log(reverseWords("The quick brown fox jumps over the lazy dog."));
 
-//   export const reverse = n => n.split("").reverse().join("");
+reverseWords("The quick brown fox jumps over the lazy dog.")  //console logging the function threw and extra 'undefined' which drove me crazy for an hour... fml
+
+//.map - new array with each element result of callback
+//.forEach - calls callbackFn and returns undefined - does not mutate the array on which it is called. (However, callbackFn may do so) - no way to stop or break a forEach() loop - does not wait for promises
