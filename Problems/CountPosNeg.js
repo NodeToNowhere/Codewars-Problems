43c06faf6f7daf2fbc2bec4bae7f60ea2898689b
Tony <NodeToNowhere@gmail.com>
2022-04-20 23:52:15 +0100

    Passing - refactor - Next time RTFQ

--- Problems/CountPosNeg.js
+++ Problems/CountPosNeg.js
@@ -1,11 +1,32 @@
 //Array sum of positive & negative
 
 export function countPosNeg(input) {
-  let array = [];
-  for (let i in input) i >= 0 ? (array[0] += i) : (array[1] += i);
-  console.log(array)
-  return array;
+  let array = [0,0]
+  for (let num in input)
+  if (input[num] > 0) {
+          array[0] += 1;
+        } else {
+          array[1] += input[num];
+        }
+  return array
 }
 
 let testData = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15];
-countPosNeg(testData)
+countPosNeg(testData);
+
+// input.forEach()           //uses 'index' and 'element'
+
+// let array = [];
+//   for (let item in input)
+//     input[item] >= 0 ? input[item] += array[0] : input[item] += array[1];
+//   return array
+
+// input.forEach(num => num > 0 ? newArray[0] += num : newArray[1] += num)
+
+// for (let num in input)
+//     console.log(input[num])
+//     if (input[num] > 0) {
+//       newArray[0] += input[num];
+//     } else {
+//       newArray[1] += input[num];
+//     }
