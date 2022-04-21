7c6774202e0577c83764df520c7db307eec48a2a
Tony <NodeToNowhere@gmail.com>
2022-04-21 15:46:39 +0100

    refactor HighLow

--- Problems/HighLow.js
+++ Problems/HighLow.js
@@ -1,14 +1,14 @@
 export function highLow(nums) {
-    let high = Math.min(...nums.split(" "))
-    let low = Math.max(...nums.split(" "))
-    return(`${low} ${high}`)
+    let high, low
+    return (`${low = Math.max(...nums.split(" "))} ${high = Math.min(...nums.split(" "))}`)
 }
-
-
 
 // highLow("8 3 -5 42 -1 0 0 -9 4 7 4 -4")
 
-// 
+
+// // let high = Math.min(...nums.split(" "))
+// let low = Math.max(...nums.split(" "))
+// return(`${low} ${high}`)
 
 // function highLow(nums){
 //     let high
