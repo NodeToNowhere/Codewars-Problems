4eb17afeebb46e52857f2d4fd6b99944bc557dde
Tony <NodeToNowhere@gmail.com>
2022-04-26 22:41:36 +0100

    Sort odds only - fixed

--- Problems/SortTheOdd.js
+++ Problems/SortTheOdd.js
@@ -3,9 +3,11 @@
 
 export function sortArray(array){
     //create array of odds - then sort
-    const odds = array.filter(num => num % 2).sort();
+    const odds = array.filter(num => num % 2).sort((a,b) => a - b);
     //map over array - if odd replace with the next sorted odd else itself
     return array.map(num => num % 2 ? odds.shift() : num)
 
 }
 
+// sort() broken without comparison function - negatives? 
+// saw you could Math.abs(num % 2) but tests passed 
