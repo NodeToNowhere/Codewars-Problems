512c873fe3479cffd948e640ecaad7d813d316f2
Tony <NodeToNowhere@gmail.com>
2022-04-28 23:51:51 +0100

    primes

--- Problems/IsPrime.js
+++ Problems/IsPrime.js
@@ -1,6 +1,9 @@
 //return true or false base on of num is prime
 // will be int - can be negative and zero - nums to 2^31
 
+//all primes odd
 export function isPrime(num){
-    
+    for(let i = 2, s = Math.sqrt(num); i <= s; i++) 
+        if(num % i === 0) return false; 
+    return num >= 2;
 }
